840f520ea8585fd6c684fc1bd2c16c217194231a
Mukesh-a <mukeshabhi08@gmail.com>
2023-06-28 21:09:49 +0530

    Updates

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: process.env.API });
 
 //Axios interceptors are functions that are called before a request is sent and after a response is received.
 //https://geshan.com.np/blog/2022/12/axios-interceptors/#:~:text=Interceptors%20can%20be%20used%20to,%2C%20transform%20the%20response%2C%20etc.
